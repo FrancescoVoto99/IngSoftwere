c054a72109d53bfd5812f0cbeb805516a205c345
Emanuela Saleggia <s1093708@studenti.univpm.it>
2021-05-22 16:43:27 +0200

    servizi

--- servizio/model/Servizio.py
+++ servizio/model/Servizio.py
@@ -1,16 +1,16 @@
 class Servizio():
 
-    def __init__(self, id, nome, tipo, reparto, posto_letto):
+    def __init__(self, id, nome, tipo, reparto, posto_letto, disponibile):
         super(Servizio, self).__init__()
         self.id = id
         self.nome = nome
         self.tipo = tipo
         self.reparto = reparto
         self.posto_letto = posto_letto
-        self.disponibile = True
+        self.disponibile = disponibile
 
     def is_disponibile(self):
-        return self.disponibile
+        self.disponibile = True
 
     def prenota(self):
         self.disponibile = False
